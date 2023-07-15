d1725f402ef72cf8c93c2ad5998d824d4dae3b39
John Castberg <jpcastberg@gmail.com>
2023-07-15 10:24:10 -0700

    Initial Commit

---
+++ radio/radioApp.swift
@@ -0,0 +1,17 @@
+//
+//  radioApp.swift
+//  radio
+//
+//  Created by John Castberg on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct radioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
